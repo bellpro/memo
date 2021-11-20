72d49915a45fe34aa6029d5c614038710efddc6f
bellpro <kju1909020@gmail.com>
2021-11-21 01:24:29 +0900

    Application (JPA)

--- src/main/java/com/bellpro/memo/MemoApplication.java
+++ src/main/java/com/bellpro/memo/MemoApplication.java
@@ -2,7 +2,9 @@ package com.bellpro.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 데이터 변동 있으면 JPA가 자동으로 반영
 @SpringBootApplication
 public class MemoApplication {
 
