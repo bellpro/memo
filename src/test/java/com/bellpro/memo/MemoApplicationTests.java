870b0ceaa62cf6456191a55e6fe6097aeaa7f114
bellpro <kju1909020@gmail.com>
2021-11-21 01:02:49 +0900

    Initial commit

---
+++ src/test/java/com/bellpro/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellpro.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
