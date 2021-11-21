523bf7132151117ad19f07830e1e1913c618a83c
bellpro <kju1909020@gmail.com>
2021-11-21 11:55:16 +0900

    index, basic, style 틀 작성

---
+++ src/main/resources/static/basic.js
@@ -0,0 +1,91 @@
+// 사용자가 내용을 올바르게 입력하였는지 확인합니다.
+function isValidContents(contents) {
+    if (contents == '') {
+        alert('내용을 입력해주세요');
+        return false;
+    }
+    if (contents.trim().length > 140) {
+        alert('공백 포함 140자 이하로 입력해주세요');
+        return false;
+    }
+    return true;
+}
+
+// 익명의 username을 만듭니다.
+function genRandomName(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        let number = Math.random() * charactersLength;
+        let index = Math.floor(number);
+        result += characters.charAt(index);
+    }
+    return result;
+}
+
+// 수정 버튼을 눌렀을 때, 기존 작성 내용을 textarea 에 전달합니다.
+// 숨길 버튼을 숨기고, 나타낼 버튼을 나타냅니다.
+function editPost(id) {
+    showEdits(id);
+    let contents = $(`#${id}-contents`).text().trim();
+    $(`#${id}-textarea`).val(contents);
+}
+
+function showEdits(id) {
+    $(`#${id}-editarea`).show();
+    $(`#${id}-submit`).show();
+    $(`#${id}-delete`).show();
+
+    $(`#${id}-contents`).hide();
+    $(`#${id}-edit`).hide();
+}
+
+function hideEdits(id) {
+    $(`#${id}-editarea`).hide();
+    $(`#${id}-submit`).hide();
+    $(`#${id}-delete`).hide();
+
+    $(`#${id}-contents`).show();
+    $(`#${id}-edit`).show();
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function () {
+    // HTML 문서를 로드할 때마다 실행합니다.
+    getMessages();
+})
+
+// 메모를 불러와서 보여줍니다.
+function getMessages() {
+    // 1. 기존 메모 내용을 지웁니다.
+    // 2. 메모 목록을 불러와서 HTML로 붙입니다.
+}
+
+// 메모 하나를 HTML로 만들어서 body 태그 내 원하는 곳에 붙입니다.
+function addHTML(id, username, contents, modifiedAt) {
+    // 1. HTML 태그를 만듭니다.
+    // 2. #cards-box 에 HTML을 붙인다.
+}
+
+// 메모를 생성합니다.
+function writePost() {
+    // 1. 작성한 메모를 불러옵니다.
+    // 2. 작성한 메모가 올바른지 isValidContents 함수를 통해 확인합니다.
+    // 3. genRandomName 함수를 통해 익명의 username을 만듭니다.
+    // 4. 전달할 data JSON으로 만듭니다.
+    // 5. POST /api/memos 에 data를 전달합니다.
+}
+
+// 메모를 수정합니다.
+function submitEdit(id) {
+    // 1. 작성 대상 메모의 username과 contents 를 확인합니다.
+    // 2. 작성한 메모가 올바른지 isValidContents 함수를 통해 확인합니다.
+    // 3. 전달할 data JSON으로 만듭니다.
+    // 4. PUT /api/memos/{id} 에 data를 전달합니다.
+}
+
+// 메모를 삭제합니다.
+function deleteOne(id) {
+    // 1. DELETE /api/memos/{id} 에 요청해서 메모를 삭제합니다.
+}
